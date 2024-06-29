1e96f59e8a530d4fa4225c9b441f2008a9f22690
haris-khandre <harishkhandre2901@gmail.com>
2024-06-30 00:49:04 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
+import { Toaster } from "@/components/ui/sonner";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Breedit",
+  description: "Ally of your pet",
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const session = await auth();
+  return (
+    <SessionProvider session={session}>
+      <html lang="en">
+        <body className={inter.className}>
+          <Navbar />
+          {children}
+          <Toaster />
+        </body>
+      </html>
+    </SessionProvider>
+  );
+}
